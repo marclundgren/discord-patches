f092be67f3e0ad241b68414298938164fc814cea
Marc Lundgren <marclundgren2.0@gmail.com>
2017-11-17 12:44:18 -0800

    removed the bot from a public repo. it now extracts from a json file

--- bot.js
+++ bot.js
@@ -9,17 +9,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 // The token of your bot - https://discordapp.com/developers/applications/me
-const token = 'MzY2NzE3NDc3MjY4NDg4MjAy.DM0iuA.voBLw3K97OGA-qnmOosYRv4jO-Y';
+// const token = 'MzY2NzE3NDc3MjY4NDg4MjAy.DM0iuA.voBLw3K97OGA-qnmOosYRv4jO-Y';
+
+const { token } = require('./token.json', { format: 'json' });
 
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
 client.on('ready', () => {
-  console.log('I am ready!');
-  console.log('I am ready!');
-  console.log('I am ready!');
-  console.log('I am ready!');
-  console.log('I am ready!');
-  console.log('I am ready!');
+  console.log('I am in charge!');
 });
 
 // Create an event listener for new guild members
